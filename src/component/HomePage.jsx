af78c781ab982d6b189828b0d30fbd042c03d0fa
efe1001 <166624089+efe1001@users.noreply.github.com>
2025-08-13 14:58:44 +0100

    f

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -88,8 +88,10 @@ function Homepage() {
   ];
 
   const longParagraphs = [
-    `Single people, listen up: If you're looking for love, want to start dating, or just keep it casual, you need to be on Tinder. With over 55 billion matches made, it's the place to be to meet your next best match. Let's be real, the dating landscape looks very different today, as most people are meeting online. With Tinder, the world's most popular free dating app, you have millions of other single people at your fingertips and they're all ready to meet someone like you. Whether you're straight or in the LGBTQIA community, Tinder's here to bring you all the sparks.`,
-    `There really is something for everyone on Tinder. Want to get into a relationship? You got it. Trying to find some new friends? Say no more. New kid on campus and looking to make the most of your college experience? Tinder U's got you covered. Tinder isn't your average dating site — it's the most diverse dating app, where adults of all backgrounds and experiences are invited to make connections, memories, and everything in between.`,
+    `Woomeout is Nigeria’s intentional dating app that takes you from “Hi” to “Let’s meet” without endless chatting. 💌
+Discover people near you, send instant date requests, or hop on a video call.
+Join weekly hangouts like beach days, karaoke nights, and game meetups — all designed to help you meet real people, make real connections, and create unforgettable moments. ❤️`,
+   // `There really is something for everyone on Tinder. Want to get into a relationship? You got it. Trying to find some new friends? Say no more. New kid on campus and looking to make the most of your college experience? Tinder U's got you covered. Tinder isn't your average dating site — it's the most diverse dating app, where adults of all backgrounds and experiences are invited to make connections, memories, and everything in between.`,
   ];
 
   const toggleStory = (i) => {
